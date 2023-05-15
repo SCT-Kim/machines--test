fa573fb613f80c2747dd3be732adb8307af42ada
SCT-Kim <133302089+SCT-Kim@users.noreply.github.com>
2023-05-15 17:22:32 +0900

    commit

---
+++ frontend/src/components/TypeInCommand.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            TypeIn
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="typeIn"
+            >
+                TypeIn
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'TypeInCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            typeIn() {
+                this.$emit('typeIn', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
